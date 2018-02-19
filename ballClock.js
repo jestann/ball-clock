7f20a93e75ada5d12aec913dcadcacc28805b372
Jess Bird <jessannbird@gmail.com>
2018-02-19 04:32:21 +0000

    developing second algorithm

--- ballClock.js
+++ ballClock.js
@@ -33,7 +33,11 @@ class BallClock {
   // INTERNAL METHODS
   
   initialize (numberOfBalls) {
-    if (numberOfBalls < 27 || numberOfBalls > 127) { return false }
+    if (numberOfBalls < 27 || numberOfBalls > 127) { 
+      let error = "ERROR: MUST CHOOSE NUMBER OF BALLS BETWEEN 27 AND 127"
+      console.log(error)
+      return { success: false, error }
+    }
     this.totalMinutes = 0
     this.numberOfBalls = numberOfBalls
     this.min = []
@@ -45,14 +49,28 @@ class BallClock {
       this.main.push(i)
       this.original.push(i)
     }
-    return true
+    return { success: true }
   }
+  
+  /* global performance */ // for linter
+  printTime (func) {
+    let start = performance.now()
+    let result = func()
+    let end = performance.now()
+    let mil = Math.floor(end - start)
+    let sec = mil/1000
+    console.log(`Completed in ${mil} milliseconds (${sec} seconds)`)
+  }
+
+
+  
+  /* NAIVE ALGORITHM */
   
   tick () {
     this.totalMinutes++
-    let releasedBall = this.main.shift()
+    let releasedBall = this.main.shift() // shift may be a costly operation
     if (this.min.length < 4) {
-      this.min.push(releasedBall)
+      this.min.push(releasedBall) // push and pop may be constant time operations
       
     } else if (this.fiveMin.length < 11) {
       while (this.min[0]) { this.main.push(this.min.pop()) }
@@ -80,91 +98,142 @@ class BallClock {
     return one.length === two.length && one.every((v, i) => (v === two[i]))
   }
 
-  countMinToRepeat () {
-    this.totalMinutes = 0
-    this.tick()
+  countMinToFullCycle (numberOfBalls) {
+    this.tick() // pass initial array equality
     while (!this.arraysEqual(this.main, this.original)) { this.tick() }
     return this.totalMinutes
   }
-    
-   getTwelveMap () {
-     this.setupBalls(this.numberOfBalls)
-     let map = this.compute(12*60)
-     return map
-   }
-   
-   getHourMap () {
-     this.setupBalls(this.numberOfBalls)
-     let map = this.compute(60)
-     return map
-   }
-   
-   get5Map () {
-     this.setupBalls(this.numberOfBalls)
-     let map = this.compute(5)
-     return map
-   }
-   
-   getMultiples (num) {
-     let twelves, hours, fives = null
-     let numLeft = num
-     if (num >= 12*60) {
-       twelves = Math.floor(num/(12*60))
-       numLeft = num % (12*60)
-     }
-     if (num >= 60) {
-       hours = Math.floor(numLeft/60)
-       numLeft = numLeft % 60
-     }
-     if (num >= 5) {
-       fives = Math.floor(numLeft/5)
-       numLeft = numLeft % 5
-     }
-     return { twelves, hours, fives, mins: numLeft }
-   }
-   
-   calculateMaps (num) {
-     let mults = this.getMultiples(num)
-     
-   }
 
-  modeOne (numberOfBalls, numberOfMinutes) {
-    let initialized = this.setupBalls(numberOfBalls)
-    if (!initialized) { 
-      console.log('Mode 1 fails.')
-      return 'ERROR: INVALID NUMBER OF BALLS'
-    }
 
-    let start = performance.now()
-    let result = JSON.stringify(this.count(numberOfMinutes))
-    let end = performance.now()
-    
-    console.log(`${numberOfBalls} balls were cycled over ${numberOfMinutes} minutes.`)
-    console.log(result)
-    let mil = Math.floor(end - start)
-    let sec = mil/1000
-    console.log(`Completed in ${mil} milliseconds (${sec} seconds)`)
+  modeOneNaive (numberOfBalls, numberOfMinutes) {
+    let initialized = this.initialize(numberOfBalls) // keep error handling at user input level
+    if (!initialized.success) { return initialized.error }
+    let result = null
+    this.printTime(() => {
+      result = JSON.stringify(this.count(numberOfBalls, numberOfMinutes))
+      console.log(`${numberOfBalls} balls were cycled over ${numberOfMinutes} minutes.`)
+      console.log(result)
+    })
     return result
   }
   
-  modeTwo (numberOfBalls) {
-    let initialized = this.setupBalls(numberOfBalls)
-    if (!initialized) { 
-      console.log('Mode 2 fails.')
-      return 'ERROR: INVALID NUMBER OF BALLS'
-    }
-
-    let start = performance.now()
-    let magic = this.countMinToRepeat()
-    let end = performance.now()
-    
-    let days = magic/(60*24)
-    console.log(`${numberOfBalls} balls cycle after ${days} days.`)
-    let mil = Math.floor(end - start)
-    let sec = mil/1000
-    console.log(`Completed in ${mil} milliseconds (${sec} seconds)`)
+  modeTwoNaive (numberOfBalls) {
+    let initialized = this.initialize(numberOfBalls) // keep error handling at user input level
+    if (!initialized.success) { return initialized.error }
+    let magic, days = null
+    this.printTime(() => {
+      magic = this.countMinToFullCycle()
+      days = magic/(60*24)
+      console.log(`${numberOfBalls} balls cycle after ${days} days.`)
+    })
     return days
   }
+  
+  
+  
+  /* ALGORITHM 2: MAP TRANSFORMATIONS */
+  
+  getMultiples (num) {
+    let twelve, sixty, five = null
+    let numLeft = num
+    if (num >= 12*60) {
+      twelve = Math.floor(num/(12*60))
+      numLeft = num % (12*60)
+    }
+    if (num >= 60) {
+      sixty = Math.floor(numLeft/60)
+      numLeft = numLeft % 60
+    }
+    if (num >= 5) {
+      five = Math.floor(numLeft/5)
+      numLeft = numLeft % 5
+    }
+    return { twelve, sixty, five, min: numLeft }
+  }
+
+  getMaps (numBalls, numMinutes) {
+    this.initialize(numBalls) // won't be an error, as this is called internally
+    let maps = this.count(numMinutes)
+    return maps
+  }
+  
+  mapTransformation (originalMain, mainMap, arrayMap=null) {
+    let main, array = []
+    if (arrayMap) { arrayMap.forEach((ballNum) => { array.push(originalMain[ballNum]) }) }
+    mainMap.forEach((ballNum) => { main.push(originalMain[ballNum]) })
+    return { main, array }
+  }
+
+  powerTranform (originals, maps, numTimes, arrayName=null) {
+    let temp = originals
+    let arrayMap = arrayName ? maps[arrayName] : null
+    for (let i = 0; i < numTimes; i++) { temp = this.mapTransformation(temp.main, maps.main, arrayMap) }
+    return temp
+  }
+  
+  transformToMin (numBalls, numMinutes) {
+    let mults = this.getMultiples(numMinutes)
+    let maps = {}
+    if (mults.twelve) { maps.twelve = this.getMaps(numBalls, 12*60) }
+    if (mults.sixty) { maps.sixty = this.getMaps(numBalls, 60) }
+    if (mults.five) { maps.five = this.getMaps(numBalls, 5) }
+    if (mults.min) { maps.min = this.getMaps(numBalls, 1) }
+    
+    this.initialize(numBalls) // clear out previous "get map" states
+    let temp = {}
+    
+    // for a number of twelve-hour cycles
+    let state = this.powerTransform( {main: this.main}, maps.twelve, mults.twelve )
+    console.log('state: ', state) // after that many 12-hour cycles
+    
+    // for a number of hours
+    temp = this.powerTransform(state, maps.sixty, mults.sixty, 'hour')
+    state = { main: temp.main, hour: temp.array }
+
+    // for a number of five-minute increments
+    temp = this.powerTransform(state, maps.five, mults.five, 'fiveMin')
+    state = { main: temp.main, hour: state.hour, fiveMin: temp.array }
+    
+    // for a number of minutes
+    temp = this.powerTransform(state, maps.min, mults.min, 'min')
+    state = { main: temp.main, hour: state.hour, fiveMin: state.fiveMin, min: temp.array }
+    
+    return state
+  }
+  
+  countCycles (numBalls) {
+    let map = this.getMaps(numBalls, 12*60) // no error handling needed here
+    
+    
+  }
+  
+  modeOne (numBalls, numMinutes) {
+    let initialized = this.initialize(numBalls) // keep error handling at user input level
+    if (!initialized.success) { return initialized.error }
+    this.printTime(() => {
+      let result = JSON.stringify(this.transformToMin(numBalls, numMinutes))
+      console.log(`${numBalls} balls were cycled over ${numMinutes} minutes.`)
+      console.log(result)
+    })
+  }
+  
+  modeTwo (numBalls) {
+    let initialized = this.initialize(numBalls) // keep error handling at user input level
+    if (!initialized.success) { return initialized.error }
+    let magic, days = null
+    this.printTime(() => {
+      magic = this.countCycles()
+      days = magic/(60*24)
+      console.log(`${numBalls} balls cycle after ${days} days.`)
+    })
+    return days
+  }
+  
+  
+  /* ALGORITHM 3 */
+  
+  
+  
 }
 
 const ballClock = new BallClock()
